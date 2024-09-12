b42cc8a5fbe2671ee2b9c7dd54ace28d1e414d35
spygaurad <suraj.prasai97@gmail.com>
2024-09-12 19:12:01 +0545

    changes in indexer

--- indexing/indexer.py
+++ indexing/indexer.py
@@ -1,14 +1,14 @@
-# from langchain.embeddings.huggingface import HuggingFaceBgeEmbeddings
-# from llama_index.embeddings import LangchainEmbedding
-# # Create the embedding model using the HuggingFaceBgeEmbeddings class
+from langchain.embeddings.huggingface import HuggingFaceBgeEmbeddings
+from llama_index.embeddings import LangchainEmbedding
+# Create the embedding model using the HuggingFaceBgeEmbeddings class
 
-# embedding_model_name = "BAAI/bge-large-en-v1.5"
+embedding_model_name = "BAAI/bge-large-en-v1.5"
 
 
-# embed_model = LangchainEmbedding(
-#   HuggingFaceBgeEmbeddings(model_name=embedding_model_name)
-# )
+embed_model = LangchainEmbedding(
+  HuggingFaceBgeEmbeddings(model_name=embedding_model_name)
+)
 
-# # Get the embedding dimension of the model by doing a forward pass with a dummy input
-# embed_dim = len(embed_model.get_text_embedding("Hello world")) # 1024
+# Get the embedding dimension of the model by doing a forward pass with a dummy input
+embed_dim = len(embed_model.get_text_embedding("Hello world")) # 1024
 
